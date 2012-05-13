f0145b9a2974baadd8f9f0222bd272e39c8641a4
Michael Sutherland <mike@codesmak.com>
2012-05-13 01:42:01 -0300

    Initial checkin of project

---
+++ header.php
@@ -0,0 +1,25 @@
+<?
+    session_start(); 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>GitPub - Were The Code Flows</title>
+
+    <!-- jquery-ui stylesheet -->
+    <link rel="stylesheet" type="text/css" media="all" href="docs/include/jqueryui/css/smoothness/jquery-ui-1.8.19.custom.css" />
+
+    <!-- jquery & jquery-ui javascript frameworks -->
+    <script rel="javascript" type="text/javascript" src="docs/include/jquery/jquery-1.7.1.min.js"></script>
+    <script rel="javascript" type="text/javascript" src="docs/include/jqueryui/js/jquery-ui-1.8.19.custom.min.js"></script>
+
+    <!-- pennywise stylesheet -->
+    <link rel="stylesheet" type="text/css" media="all" href="docs/css/gitpub.css" />
+
+    <!-- pennywise javascript library -->
+    <script rel="javascript" type="text/javascript" src="docs/javascript/gitpub.js"></script>
+
+</head>
+<body>
