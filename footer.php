dbe1a1ddf4845ae83013c9d3b60cbd28f290eed5
Michael Sutherland <mike@codesmak.com>
2012-05-19 00:06:32 -0300

    - updated footer section and css

--- footer.php
+++ footer.php
@@ -1,11 +1,18 @@
     <div class="footer">
         <div class="content">
-            <div class="right">
+            <div class="section right">
                 Thanks ;)
                 <ul>
                     <li><a href="http://fimml.at/#glip" target="_blank">Glip - php library for git</a></li>
                     <li><a href="http://git-scm.com/" target="_blank">Git - distributed version control system</a></li>
                     <li><a href="https://github.com/" target="_blank">Github - social coding</a></li>
+                    <li><a href="http://php.net/" target="_blank">PHP</a></li>
+                </ul>
+            </div>
+            <div class="section right">
+                About
+                <ul>
+                    <li><a href="http://codesmak.com" target="_blank">codesmak.com</a></li>
                 </ul>
             </div>
 
