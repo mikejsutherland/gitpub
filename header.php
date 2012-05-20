900e823ee6cbabced77cd49fb10cf5a863967482
Michael Sutherland <mike@codesmak.com>
2012-05-20 15:13:51 -0300

    - fixed comments and commented out jqueryui as its not used

--- header.php
+++ header.php
@@ -34,21 +34,21 @@
     <title>GitPub - Were The Code Flows</title>
 
     <!-- jquery-ui stylesheet -->
-    <link rel="stylesheet" type="text/css" media="all" href="docs/include/jqueryui/css/smoothness/jquery-ui-1.8.19.custom.css" />
+    <!-- <link rel="stylesheet" type="text/css" media="all" href="docs/include/jqueryui/css/smoothness/jquery-ui-1.8.19.custom.css" /> -->
 
     <!-- jquery & jquery-ui javascript frameworks -->
     <script rel="javascript" type="text/javascript" src="docs/include/jquery/jquery-1.7.1.min.js"></script>
-    <script rel="javascript" type="text/javascript" src="docs/include/jqueryui/js/jquery-ui-1.8.19.custom.min.js"></script>
+    <!-- <script rel="javascript" type="text/javascript" src="docs/include/jqueryui/js/jquery-ui-1.8.19.custom.min.js"></script> -->
 
-    <!-- -->
+    <!-- history.js -->
     <script rel="javascript" type="text/javascript" src="docs/include/history.js/jquery.history.js"></script>      
     <script rel="javascript" type="text/javascript" src="docs/include/history.js/jquery.scrollto.min.js"></script>
     <script rel="javascript" type="text/javascript" src="docs/include/history.js/ajaxify-html5.js"></script>
 
-    <!-- pennywise stylesheet -->
+    <!-- gitpub stylesheet -->
     <link rel="stylesheet" type="text/css" media="all" href="docs/css/gitpub.css" />
 
-    <!-- pennywise javascript library -->
+    <!-- gitpub javascript library -->
     <script rel="javascript" type="text/javascript" src="docs/javascript/gitpub.js"></script>
 
 </head>
