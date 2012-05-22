363911f6bd444a60b0b08a747d15196aa32dd846
Michael Sutherland <mike@codesmak.com>
2012-05-22 13:29:02 -0300

    - disabled clone url for now

--- index.php
+++ index.php
@@ -2,10 +2,19 @@
 
     <div class="masthead gradient_gray">
         <div class="content">
+
+            <!-- Main gitpub link -->
             <a href="<?=$CONFIG['base_uri'];?>/">gitpub</a>
+
 <? if ( isset($_SESSION['repo']) && $_SESSION['repo'] !== "" ) { ?>
+
+            <!-- HTTP clone url -->
+            <!--
             <input class="repourl" value="<?='http://'.$_SERVER['HTTP_HOST'].$CONFIG['base_uri'].'/r/'.$_SESSION['repo']?>" spellcheck="false" readonly="readonly" />
             <span class="urlbox gradient_gray">HTTP</span>
+            -->
+
+            <!-- Display the Branch -->
             <span class="urlbox gradient_gray">branch: <em><?=$_SESSION['GIT']['branch'];?></em></span>
             <br class="clear" />
 <? } ?>
