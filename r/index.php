e6b4ea74d37ddf903833783251ae21b0c3d75b42
Michael Sutherland <mike@codesmak.com>
2012-06-16 16:08:48 -0300

    - Initial quasi-working HTTP clone proxy in php

--- r/index.php
+++ r/index.php
@@ -1,4 +1,14 @@
 <?
+    if ( empty($_GET['service']) ) {
+        header('Content-Type', 'text/plain');
+    }
+    else {
+        header('Content-Type', "application/x-". $_GET['service'] ."-advertisement");
+    }
+    header('Expires', 'Fri, 01 Jan 1980 00:00:00 GMT');
+    header('Pragma', 'no-cache');
+    header('Cache-Control', 'no-cache, max-age=0, must-revalidate');
+
    /*
     * This file will with the help of the associated .htaccess file
     * return any corresponding git repo file, as would be requested
@@ -6,61 +16,76 @@
     *
     */
 
-    # Load the config file so we know where the repo's are
-    include_once('config/gitpub.config.php');
+    $thispath = realpath(dirname(__FILE__).DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR;
 
-    debug($_GET['l']);
+    require_once($thispath . '../config/gitpub.config.php');
 
-    if ( preg_match("/git/", $_SERVER['HTTP_USER_AGENT']) ) {
+    $repo = null;
+    $request = null;
 
-        # Set the path of the requested file
-        $requested_file = $CONFIG['repo_directory'] .'/'. $_GET['l'];
+    if ( preg_match("/^([^\/]+)\/(.+)$/", $_GET['uri'], $matches) ) {
 
-        if ( file_exists($requested_file) ) {
+        $repo = $matches[1];
+        $request = $matches[2];
+    }
+    elseif ( preg_match("/^([^\/]+)/", $_GET['uri'], $matches) ) {
 
-            # Set the content-type
-            header("Content-type: ". mime_content_type($requested_file) ."\n\n");
-
-            $fp = fopen($requested_file, 'rb');
-            fpassthru($fp);
-            #print file_get_contents($requested_file);
-        }
-        elseif ( preg_match("/(.+)\/info\/refs$/", $requested_file, $repo) ) {
-
-            $cmd = "/usr/local/git/bin" . "/git-upload-pack '". "/Volumes/Development/git/gitpub/" . "'";
-            debug("running: $cmd");
-            exec($cmd);
-
-        }
+        $repo = $matches[1];
+        $request = (isset($matches[2])) ? $matches[2] : null;
     }
 
-    # Redirect to parent if request didn't come from git
+    // Redirect on empty request
+    if ( empty($request) ) {
+        header("Location: ". $CONFIG['base_uri'] .'/');
+    }
     else {
 
-        header("Location: ". $CONFIG['base_uri'] .'/');
+        // Reject any file request which contains parent notation
+        if ( preg_match("/\.\./", $request) ) {
+            print "die die die\n";
+            exit;
+        }
 
-    }
+        // Basic user-agent check
+        if ( preg_match("/git/", $_SERVER['HTTP_USER_AGENT']) ) {
 
+            $repo_path = $CONFIG['projects_dir'] ."/$repo"; 
+            $repo_path .= is_dir($repo_path ."/refs") ? "" : is_dir($repo_path ."/.git/refs") ? "/.git" : "";
 
-    function getMimeType($filename) {
+            $requested_file = $repo_path ."/". $request;
 
-        if ( function_exists('finfo_open') ) {
-            $finfo = finfo_open(FILEINFO_MIME);
-            $mimetype = finfo_file($finfo, $filename);
-            finfo_close($finfo);
-            return $mimetype;
+            if ( preg_match("/^info\/refs/", $request) ) {
+
+                $branches = scandir($repo_path ."/refs/heads");
+
+                foreach ($branches as $branch) {
+
+                    $requested_file = $repo_path ."/refs/heads/$branch";
+
+                    if ( $branch == '.' || $branch == '..' || ! is_file("$requested_file") ) { continue; }
+
+                    print trim(file_get_contents($requested_file)) ."\trefs/heads/$branch\n";
+                }
+            }
+            #elseif ( preg_match("/^HEAD$/", $request) ) {
+
+            #    print "ref: refs/heads/master\n";
+            #}
+            elseif ( file_exists($requested_file) ) {
+
+                $fp = fopen($requested_file, 'rb');
+                fpassthru($fp);
+            }
+            else {
+
+                print "die die die\n";
+                exit;
+            }
         }
         else {
-            return 'application/octet-stream';
+
+            header("Location: ". $CONFIG['base_uri'] .'/');
         }
-    }
-
-    function debug($msg) {
-
-        $file = "gitpup.log";
-        $fh = fopen($file, 'a') or die("can't open file");
-        fwrite($fh, "$msg\n");
-        fclose($fh);
     }
 
 ?>
