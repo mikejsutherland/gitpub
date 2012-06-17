9e58a5cf4938d3e04c919e211f6e239ef17369f4
Michael Sutherland <mike@codesmak.com>
2012-06-16 22:59:59 -0300

    - added GNU license header to each file

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
-<?  include('header.php'); ?>
+<?
+   /*
+    * Copyright (c) 2012 codesmak.com
+    *
+    * This file is part of gitpub.
+    *
+    * gitpub is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    *
+    * gitpub is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU General Public License for more details.
+    *
+    * You should have received a copy of the GNU General Public License
+    * along with gitpub.  If not, see <http://www.gnu.org/licenses/>.
+    *
+    */
+
+    include('header.php');
+?>
 
     <div class="masthead gradient_gray">
         <div class="content">
--- views/repos.php
+++ views/repos.php
@@ -1,5 +1,26 @@
-<?  if ( count($gp->repos) > 0 ) { ?><?="\n";?>
+<?
+   /*
+    * Copyright (c) 2012 codesmak.com
+    *
+    * This file is part of gitpub.
+    *
+    * gitpub is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    *
+    * gitpub is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU General Public License for more details.
+    *
+    * You should have received a copy of the GNU General Public License
+    * along with gitpub.  If not, see <http://www.gnu.org/licenses/>.
+    *
+    */
 
+    if ( count($gp->repos) > 0 ) {
+?>
             <div class="box corners">
                 <div class="boxhead gradient_aqua">1 person hosting over <?=(count($gp->repos)-1);?>+ repositories</div>
                 <div class="boxbody">
