7e57db850703bef370c2b46114cf0b22c2a4359c
Michael Sutherland <mike@codesmak.com>
2012-06-16 23:11:05 -0300

    - added example config

---
+++ config/example.gitpub.config.php
@@ -0,0 +1,17 @@
+<?
+
+    ## gitpub web settings ##
+    $CONFIG['base_uri'] = '/gitpub';
+    $CONFIG['commits_per_page'] = 15;
+    $CONFIG['enable_cache'] = false;
+    $CONFIG['enable_clone'] = true;
+
+    ## paths ##
+    $CONFIG['projects_dir'] = '/Volumes/Development/git';
+    $CONFIG['git_path'] = '/usr/local/git/bin';
+
+    ## php settings ##
+    #error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+    #ini_set('error_reporting', E_ALL);
+    #ini_set("memory_limit","50M");
+?>
