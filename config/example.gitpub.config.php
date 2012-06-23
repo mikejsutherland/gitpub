555194bc0cf8e3451fd0439d8d9f74d169e0d98a
Michael Sutherland <mike@codesmak.com>
2012-06-22 23:04:26 -0300

    Fixed wording of readme and example config file

--- config/example.gitpub.config.php
+++ config/example.gitpub.config.php
@@ -1,17 +1,55 @@
 <?
-
-    ## gitpub web settings ##
+    #######################################################################
+    # GitPub Settings:
+    #
+    # URI where gitpub will be served from (no trailing slash)
+    #
     $CONFIG['base_uri'] = '/gitpub';
+
+    # Set the path to the directory where you keep your
+    # git repositories
+    #
+    $CONFIG['projects_dir'] = '/Volumes/Development';
+
+    # Set the path to git (no trailing slash)
+    #
+    $CONFIG['git_path'] = '/usr/bin';
+
+    # Number of commits to show per page
+    #
     $CONFIG['commits_per_page'] = 15;
+
+    # Caching -- requires a cache/ directory to be created
+    # and given write permissions
+    #
     $CONFIG['enable_cache'] = false;
+
+    # Allow cloning over http -- requires post-update hook to be
+    # enabled. (cp hooks/post-update.sample hooks/post-update)
+    #
     $CONFIG['enable_clone'] = true;
 
-    ## paths ##
-    $CONFIG['projects_dir'] = '/Volumes/Development/git';
-    $CONFIG['git_path'] = '/usr/local/git/bin';
+    #######################################################################
+    # PHP Settings:
+    #
+    # The below php settings can be adjusted here if you are
+    # experiencing odd behavior from gitpub. If all goes well
+    # you will should not need them.
+    #
 
-    ## php settings ##
+    # Increase the error reporting by uncommenting the below
+    # two lines.
+    #
     #error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
     #ini_set('error_reporting', E_ALL);
+
+    # Large files can cause out of memory exhausted errors
+    # The memory limit can be adjusted here.
+    #
     #ini_set("memory_limit","50M");
+
+    # Disable output compression if tag downloads are corrupt
+    #
+    #ini_set('zlib.output_compression', 'Off');
+
 ?>
