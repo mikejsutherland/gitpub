e7c8cf0d783ec812977352c0a9b5ffb3d9dd015f
Michael Sutherland <mike@codesmak.com>
2012-07-03 01:23:02 -0300

    Fixed incorrect lib path in http clone proxy

--- r/index.php
+++ r/index.php
@@ -29,7 +29,7 @@
     $thispath = realpath(dirname(__FILE__).DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR;
 
     require_once($thispath . '../config/gitpub.config.php');
-    require_once($thispath . 'lib/gitpub/gitpub.extras.php');
+    require_once($thispath . '../lib/gitpub/gitpub.extras.php');
 
     // Redirect if this feature is disabled
     if ( ! $CONFIG['enable_clone'] || ! has_mod_rewrite() ) {
